e0941e9f544ca081c2a8e6639d3dd0361a7bea3b
Gwen Coffy <gwen@otavu.com>
2022-01-26 11:58:27 +0100

    new: ui: [component] add a way for a user to validate a pending account

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Profile from "../views/Profile.vue";
 import Login from "../views/Login.vue";
 import CreateMyAccount from "../views/CreateMyAccount.vue";
+import Administration from "../views/Administration.vue";
+import PendingAccounts from "../components/admin/PendingAccounts.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,6 +23,20 @@ const routes: Array<RouteRecordRaw> = [
     name: "Create my account",
     meta: { title: "Créer mon portefeuille" },
     component: CreateMyAccount,
+  },
+  {
+    path: "/admin",
+    name: "Administration",
+    meta: { title: "Administration" },
+    component: Administration,
+    children: [
+      {
+        path: "pending-accounts",
+        name: "Validate pending accounts",
+        meta: { title: "Validation des comptes en attente de création" },
+        component: PendingAccounts,
+      },
+    ]
   },
 ];
 

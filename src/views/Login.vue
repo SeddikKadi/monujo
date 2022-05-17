1c2485ca053f8a1afd59c1a94a6f49ff68a980d9
Seddik Kadi <seddik.kadi@outlook.fr>
2022-05-17 16:18:16 +0200

    new: add create account and reset password buttons (fixes: #224)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -24,7 +24,7 @@
               </span>
             </p>
           </div>
-          <div class="field mb-5">
+          <div class="field mb-2">
             <p class="control has-icons-left">
               <input
                 v-model="password"
@@ -37,12 +37,30 @@
               </span>
             </p>
           </div>
-          <div class="field">
-            <p class="control has-text-centered">
-              <button @click="load" type="submit" class="button is-login">
-                Se connecter
-              </button>
-            </p>
+          <div class="forgot-password mb-4">
+            <button @click="BaseUrl()" type="button">
+              Mot de passe oublié ?
+            </button>
+          </div>
+          <div class="login-buttons">
+            <div>
+              <p class="control has-text-centered">
+                <button @click="load" type="submit" class="button is-login">
+                  Se connecter
+                </button>
+              </p>
+            </div>
+            <div>
+              <p class="control has-text-centered">
+                <button
+                  @click="createAccountRedirect()"
+                  type="button"
+                  class="button is-login"
+                >
+                  Créer un compte
+                </button>
+              </p>
+            </div>
           </div>
           <p class="has-text-danger has-text-centered" v-if="fail">
             {{ fail }}
@@ -77,6 +95,12 @@
     methods: {
       load(): void {
         this.isLoading = true
+      },
+      BaseUrl(): void {
+        window.open(this.$store.getters.getOdooUrl() + "/web/reset_password")
+      },
+      createAccountRedirect(): void {
+        window.open(this.$store.getters.getOdooUrl() + "/web/signup")
       },
       async submit(): Promise<void> {
         try {
